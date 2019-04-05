8b0bc5cade83755d17142d6c65b84d896e581564
David Mathis <dbmathis@gmail.com>
2019-04-05 19:45:12 -0400

    added some code

--- fill-memory.sh
+++ fill-memory.sh
@@ -6,10 +6,23 @@
 # curl -sX GET $APP/info/memory/used_memory                                                                                                                                              # curl -sX GET $APP/info/memory/maxmemory
 # curl -sX GET $APP/info/stats/evicted_keys
 
-
 export APP=redis-example-app.cfapps-17.haas-59.pez.pivotal.io
 
-key=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
-value=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 | xargs)
+# Define globals
+REDISMAXMEM=$(curl -sX GET $APP/info/memory/maxmemory)
+REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory)
 
-curl -X PUT "${APP}/${key}" -d "data=${value}"
+while [ $REDISUSEDMEM -le $REDISMAXMEM ]; do
+   key=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
+   value=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 250000 | xargs)
+
+   curl -X PUT "${APP}/${key}" -d "data=${value}"
+
+   echo ''
+
+   REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory)
+   echo "Used:   ${REDISUSEDMEM}"
+   echo "Max:    ${REDISMAXMEM}"
+done
+
+
