84e8328c23cbb408330c11fa740f0b9f06375452
David Mathis <dbmathis@gmail.com>
2019-04-05 20:41:23 -0400

    worked on fill script

--- fill-memory.sh
+++ fill-memory.sh
@@ -9,20 +9,30 @@
 export APP=redis-example-app.cfapps-17.haas-59.pez.pivotal.io
 
 # Define globals
+REDISMAXMEMHUMAN=$(curl -sX GET $APP/info/memory/maxmemory_human)
 REDISMAXMEM=$(curl -sX GET $APP/info/memory/maxmemory)
+REDISUSEDMEMHUMAN=$(curl -sX GET $APP/info/memory/used_memory_human)
 REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory)
+CURDIR=${PWD} 
+value=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 10000 | xargs)
 
-while [ $REDISUSEDMEM -le $REDISMAXMEM ]; do
+echo 'SET foo bar' > "${CURDIR}/data.txt"
+
+while [ $(wc -c < "${CURDIR}/data.txt") -le $REDISMAXMEM ]; do
    key=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
-   value=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 250000 | xargs)
 
-   curl -X PUT "${APP}/${key}" -d "data=${value}"
+   echo "SET ${key} ${value}"  >> "${CURDIR}/data.txt"
+   #echo $(wc -c < "${CURDIR}/data.txt")
+   
+   #curl -X PUT "${APP}/${key}" -d "data=${value}"
 
-   echo ''
+   #echo ''
 
-   REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory)
-   echo "Used:   ${REDISUSEDMEM}"
-   echo "Max:    ${REDISMAXMEM}"
+   #REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory)
+   #REDISUSEDMEM=$(curl -sX GET $APP/info/memory/used_memory_human)
+   #echo "Used:   ${REDISUSEDMEM}"
+   #echo "Max:    ${REDISM"
 done
 
+cat "${CURDIR}/data.txt" | redis-cli -h 10.193.82.145 -a 'MBAreVyrigEBTMcFq06kyxVRTD0=' --pipe
 
